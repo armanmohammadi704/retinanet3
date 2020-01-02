debba1f102ad261f522bf0e7b3de1a94e923adfe
armanmohammadi704 <59266965+armanmohammadi704@users.noreply.github.com>
2020-01-02 13:21:35 +0330

    Add files via upload

--- keras_retinanet/preprocessing/csv_generator.py
+++ keras_retinanet/preprocessing/csv_generator.py
@@ -21,11 +21,10 @@ from ..utils.image import read_image_bgr
 import numpy as np
 from PIL import Image
 from six import raise_from
-
+import cv2
 import csv
 import sys
 import os.path
-from collections import OrderedDict
 
 
 def _parse(value, function, fmt):
@@ -45,7 +44,7 @@ def _parse(value, function, fmt):
 def _read_classes(csv_reader):
     """ Parse the classes file given by csv_reader.
     """
-    result = OrderedDict()
+    result = {}
     for line, row in enumerate(csv_reader):
         line += 1
 
@@ -64,7 +63,7 @@ def _read_classes(csv_reader):
 def _read_annotations(csv_reader, classes):
     """ Read annotations from the csv_reader.
     """
-    result = OrderedDict()
+    result = {}
     for line, row in enumerate(csv_reader):
         line += 1
 
@@ -205,7 +204,89 @@ class CSVGenerator(Generator):
     def load_image(self, image_index):
         """ Load an image at the image_index.
         """
-        return read_image_bgr(self.image_path(image_index))
+        img_path = self.image_names[image_index]
+            
+        if  img_path[-6:] == '01.jpg':
+            image_index1 = image_index
+            image_index2 = image_index
+            image_index3 = image_index
+            image_index4 = image_index
+            image_index5 = image_index+1
+            image_index6 = image_index+2
+            image_index7 = image_index+3
+        elif  img_path[-6:] == '02.jpg':
+            image_index1 = image_index-1
+            image_index2 = image_index-1
+            image_index3 = image_index-1
+            image_index4 = image_index
+            image_index5 = image_index+1
+            image_index6 = image_index+2
+            image_index7 = image_index+3
+        elif  img_path[-6:] == '03.jpg':
+            image_index1 = image_index-2
+            image_index2 = image_index-2
+            image_index3 = image_index-1
+            image_index4 = image_index
+            image_index5 = image_index+1
+            image_index6 = image_index+2
+            image_index7 = image_index+3
+        elif img_path[-6:] == '25.jpg':
+            image_index1 = image_index-3
+            image_index2 = image_index-2
+            image_index3 = image_index-1
+            image_index4 = image_index
+            image_index5 = image_index
+            image_index6 = image_index
+            image_index7 = image_index
+        elif img_path[-6:] == '24.jpg':
+            image_index1 = image_index-3
+            image_index2 = image_index-2
+            image_index3 = image_index-1
+            image_index4 = image_index
+            image_index5 = image_index+1
+            image_index6 = image_index+1
+            image_index7 = image_index+1
+        elif img_path[-6:] == '23.jpg':
+            image_index1 = image_index-3
+            image_index2 = image_index-2
+            image_index3 = image_index-1
+            image_index4 = image_index
+            image_index5 = image_index+1
+            image_index6 = image_index+2
+            image_index7 = image_index+2
+        else:
+            image_index1 = image_index-3
+            image_index2 = image_index-2
+            image_index3 = image_index-1
+            image_index4 = image_index
+            image_index5 = image_index+1
+            image_index6 = image_index+2
+            image_index7 = image_index+3
+
+        image1=read_image_bgr(self.image_path(image_index1))
+        image2=read_image_bgr(self.image_path(image_index2))
+        image3=read_image_bgr(self.image_path(image_index3))
+        image4=read_image_bgr(self.image_path(image_index4))
+        image5=read_image_bgr(self.image_path(image_index5))
+        image6=read_image_bgr(self.image_path(image_index6))
+        image7=read_image_bgr(self.image_path(image_index7))
+        gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+        gray3 = cv2.cvtColor(image3, cv2.COLOR_BGR2GRAY)
+        gray4 = cv2.cvtColor(image4, cv2.COLOR_BGR2GRAY)
+        gray5 = cv2.cvtColor(image5, cv2.COLOR_BGR2GRAY)
+        gray6 = cv2.cvtColor(image6, cv2.COLOR_BGR2GRAY)
+        gray7 = cv2.cvtColor(image7, cv2.COLOR_BGR2GRAY)
+        img = np.zeros((576,768,7))
+        img[:,:,0]= gray1
+        img[:,:,1]= gray2
+        img[:,:,2]= gray3
+        img[:,:,3]= gray4
+        img[:,:,4]= gray5
+        img[:,:,5]= gray6
+        img[:,:,6]= gray7
+        
+        return img
 
     def load_annotations(self, image_index):
         """ Load annotations for an image_index.
